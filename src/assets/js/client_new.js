469d8e6c40cfb3cd69ea45f46e00b62955da5e16
Iseden <isedenwildfire@gmail.com>
2024-10-15 21:45:29 -0500

    proper local string

--- src/assets/js/client_new.js
+++ src/assets/js/client_new.js
@@ -394,7 +394,7 @@ async function generateWorkerHireCard() {
             if (workers.global[worker] == null) continue;
             let option = document.createElement("option");
             option.value = worker;
-            option.innerText = `${worker} - ⵇ ${workers.global[worker].toLocaleString()}`;
+            option.innerText = `${worker} - ⵇ ${workers.global[worker].toLocaleString({ style: 'currency', currency: 'USD' })}`;
             thisSelect.appendChild(option);
             totalWorkers += 1;
         }
